1dcaa74451cf74e1f16857c308a869868ef0bc9c
ziyaad18 <ziyaadpetersen347@gmail.com>
2020-09-11 15:46:35 +0200

    first try

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+num1=2
+num2=4
+answer=num1+num2
+print(answer)
